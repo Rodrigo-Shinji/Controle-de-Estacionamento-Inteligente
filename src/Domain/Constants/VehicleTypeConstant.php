0c829544f1907a27c15068615cd21b144014ab08
Rodrigo Shinji Yamashita <rodrigoyamashita02@gmail.com>
2025-11-14 17:36:36 -0300

    Add files via upload

---
+++ src/Domain/Constants/VehicleTypeConstant.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Constants;
+
+final class VehicleTypeConstant
+{
+    public const CAR = 'carro';
+    public const MOTO = 'moto';
+    public const TRUCK = 'caminhao';
+
+    /**
+     *@return string[]
+     */
+    public static function getTypes(): array
+    {
+        return [self::CAR, self::MOTO, self::TRUCK];
+    }
+
+    public static function isValid(string $type): bool
+    {
+        return in_array(strtolower(trim($type)), self::getTypes(), true);
+    }
+}
